c2558e170f72708772ef9f6211df33c70d189ec2
doctor4t <25477005+doctor4t@users.noreply.github.com>
2020-12-30 14:46:24 +0100

    Delete uninstaller on game start

--- src/main/java/ladysnake/vanguard/Vanguard.java
+++ src/main/java/ladysnake/vanguard/Vanguard.java
@@ -1,8 +1,13 @@
 package ladysnake.vanguard;
 
 import net.fabricmc.api.ModInitializer;
+import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class Vanguard implements ModInitializer {
     static final String MODID = "vanguard";
@@ -12,5 +17,13 @@ public class Vanguard implements ModInitializer {
 
     @Override
     public void onInitialize() {
+        // delete uninstaller
+        if (Files.exists(Paths.get("mods/" + UNINSTALLER))) {
+            try {
+                Files.delete(Paths.get("mods/" + UNINSTALLER));
+            } catch (IOException e) {
+                logger.log(Level.WARN, "Could not remove uninstaller because of I/O Error: " + e.getMessage());
+            }
+        }
     }
 }
