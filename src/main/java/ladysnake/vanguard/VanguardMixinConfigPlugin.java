9f215fb4e3aed54bd7b0be9c3d899f401700a1cd
doctor4t <25477005+doctor4t@users.noreply.github.com>
2021-01-12 23:41:46 +0100

    Add mixin config to not crash without modmenu

---
+++ src/main/java/ladysnake/vanguard/VanguardMixinConfigPlugin.java
@@ -0,0 +1,52 @@
+package ladysnake.vanguard;
+
+import net.fabricmc.loader.api.FabricLoader;
+import org.objectweb.asm.tree.ClassNode;
+import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
+import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
+
+import java.util.List;
+import java.util.Set;
+
+public class VanguardMixinConfigPlugin implements IMixinConfigPlugin {
+    @Override
+    public void onLoad(String mixinPackage) {
+
+    }
+
+    @Override
+    public String getRefMapperConfig() {
+        return null;
+    }
+
+    @Override
+    public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
+        boolean modmenuloaded = FabricLoader.getInstance().isModLoaded("modmenu");
+
+        if (mixinClassName.equals("ladysnake.vanguard.mixin.TitleScreenMixin")) {
+            return modmenuloaded;
+        } else {
+            return true;
+        }
+    }
+
+    @Override
+    public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) {
+
+    }
+
+    @Override
+    public List<String> getMixins() {
+        return null;
+    }
+
+    @Override
+    public void preApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+
+    }
+
+    @Override
+    public void postApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+
+    }
+}
